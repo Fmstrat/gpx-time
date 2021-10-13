48b22b715843f867706c2de72a44827403e7f4a1
Fmstrat <nospam@nowsci.com>
2021-10-13 16:47:18 -0400

    Change saved filename

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ async function main() {
 
     var builder = new xml2js.Builder();
     var xml = builder.buildObject(gpxxml);
-    await fs.writeFileSync(`${process.argv[2]}2.gpx`, xml);
+    await fs.writeFileSync(process.argv[2], xml);
 }
 
 main()
